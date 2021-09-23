f72b8f8278797659f27130924a53a62a3c5ab95a
unknown <mada.lawliet@gmail.com>
2021-09-23 11:22:07 +0700

    Improving Login Page UI

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,12 +1,27 @@
+import Button from "../Button";
 import Navbar from "../Navbar";
 import styles from "./Login.module.css";
 
 const Login = () => {
   return (
     <>
-     <Navbar /> 
+      <Navbar />
+      <div className={styles.container}>
+        <form>
+          <h2>Login</h2>
+          <div className={styles.input__container}>
+            <label for="email">Email: </label>
+            <input name="email" type="text" />
+          </div>
+          <div className={styles.input__container}>
+            <label for="password">Password: </label>
+            <input name="password" type="password" />
+          </div>
+          <Button variant="primary" title="Login" />
+        </form>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
